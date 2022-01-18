1e45d938db5901deefdb1252201bb27f0a9c44cd
Kooreis <kooreis.official@gmail.com>
2022-01-18 10:00:00 -0600

    Imports necessary libraries for the program.

--- Quote_of_the_Day_Fetcher.java
+++ Quote_of_the_Day_Fetcher.java
@@ -1,36 +1,5 @@
-```java
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-
-public class QuoteFetcher {
-    private static final String API_URL = "https://api.quotable.io/random";
-
-    public static void main(String[] args) {
-        try {
-            URL url = new URL(API_URL);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("Accept", "application/json");
-
-            if (connection.getResponseCode() != 200) {
-                throw new RuntimeException("Failed : HTTP error code : " + connection.getResponseCode());
-            }
-
-            BufferedReader br = new BufferedReader(new InputStreamReader((connection.getInputStream()), StandardCharsets.UTF_8));
-            String output;
-            System.out.println("Quote of the day: ");
-            while ((output = br.readLine()) != null) {
-                System.out.println(output);
-            }
-
-            connection.disconnect();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-}
-```
