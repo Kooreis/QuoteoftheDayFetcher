1222fbced9a9295e5cf3a3b413613e64a5bdb9b9
Kooreis <kooreis.official@gmail.com>
2022-01-18 10:00:00 -0600

    Imported necessary libraries: requests for making HTTP requests and json for handling JSON data.

--- Quote_of_the_Day_Fetcher.py
+++ Quote_of_the_Day_Fetcher.py
@@ -1,14 +1,2 @@
-```python
 import requests
 import json
-
-def fetch_quote():
-    response = requests.get("https://quotes.rest/qod")
-    data = json.loads(response.text)
-    quote = data['contents']['quotes'][0]['quote']
-    author = data['contents']['quotes'][0]['author']
-    return f'"{quote}" - {author}'
-
-if __name__ == "__main__":
-    print(fetch_quote())
-```
